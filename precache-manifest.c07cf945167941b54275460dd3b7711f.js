cb9dd1879c59021ac36c3211d8a5a4253b18ad7e
yashrajsingh11 <48989081+yashrajsingh11@users.noreply.github.com>
2019-09-13 19:28:44 +0530

    Updates

---
+++ precache-manifest.c07cf945167941b54275460dd3b7711f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af13d4dca6ee546bd2de09b88285d8f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "071670445acc323b0fcd",
+    "url": "/robofriends/static/css/main.da096576.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "071670445acc323b0fcd",
+    "url": "/robofriends/static/js/main.188c018b.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
